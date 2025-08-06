2a469483d38966edc80af7d13d165a21ec986ba3
J-Breno <joao.breno85@hotmail.com>
2025-08-06 12:04:55 -0300

    Conhecendo HTTTP status code

--- 01-fundamentos-do-node/src/server.js
+++ 01-fundamentos-do-node/src/server.js
@@ -7,8 +7,8 @@ const server = http.createServer((req, res) => {
 
   if (method === "GET" && url === "/users") {
     return res
-    .setHeader('Content-type', 'application/json')
-    .end(JSON.stringify(users));
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
@@ -17,9 +17,9 @@ const server = http.createServer((req, res) => {
       name: "João Breno",
       email: "breno@gmail.com",
     });
-    return res.end("Criação de usuários");
+    return res.writeHead(201).end();
   }
-  return res.end("Hello, world");
+  return res.writeHead(404).end();
 });
 
 server.listen(3333);
