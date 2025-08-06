a37d5a6c8617da8427977fe743c6ebb79566a905
J-Breno <joao.breno85@hotmail.com>
2025-08-06 11:57:48 -0300

    Salvando usuário em memória (headers)

--- 01-fundamentos-do-node/src/server.js
+++ 01-fundamentos-do-node/src/server.js
@@ -1,14 +1,24 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
-  if (method === "GET" && url === "/users")
-    return res.end("Listagem de usuários");
+  if (method === "GET" && url === "/users") {
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users));
+  }
 
-  if (method === "POST" && url === "/users")
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "João Breno",
+      email: "breno@gmail.com",
+    });
     return res.end("Criação de usuários");
-
+  }
   return res.end("Hello, world");
 });
 
