2d86339a99e5f05063ba799a4fe444f149fd31c4
J-Breno <joao.breno85@hotmail.com>
2025-08-07 02:50:51 -0300

    Criando banco de dados JSON

--- 01-fundamentos-do-node/src/server.js
+++ 01-fundamentos-do-node/src/server.js
@@ -1,14 +1,16 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
+import { Database } from "./middlewares/database.js";
 
-const users = [];
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  await json(req. res)
+  await json(req.res);
 
   if (method === "GET" && url === "/users") {
+    const users = database.select("users");
     return res
       .setHeader("Content-type", "application/json")
       .end(JSON.stringify(users));
@@ -16,11 +18,12 @@ const server = http.createServer(async (req, res) => {
 
   if (method === "POST" && url === "/users") {
     const { name, email } = body;
-    users.push({
+    const user = {
       id: 1,
       name: name,
       email: email,
-    });
+    };
+    database.insert("users", user);
     return res.writeHead(201).end();
   }
   return res.writeHead(404).end();
