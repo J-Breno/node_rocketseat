86b2104168c1fc2455adc654db8d73d4a46f2ade
J-Breno <joao.breno85@hotmail.com>
2025-08-08 08:34:20 -0300

    Configurando banco de testes

--- 02-api-rest-nodejs/test/transactions.spec.ts
+++ 02-api-rest-nodejs/test/transactions.spec.ts
@@ -1,4 +1,5 @@
-import { it, beforeAll, afterAll, expect } from 'vitest'
+import { it, beforeAll, afterAll, expect, beforeEach } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
 import { describe } from 'node:test'
@@ -10,6 +11,11 @@ describe('Transaction routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
