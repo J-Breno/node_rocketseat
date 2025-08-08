cda053c9be3d3c866af617d988c0355bf8182ed7
J-Breno <joao.breno85@hotmail.com>
2025-08-08 15:59:42 -0300

    Controller de registro

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,24 +1,6 @@
 import fastiy from 'fastify'
-import { z } from 'zod'
-import { prisma } from './lib/prisma.js'
+import { appRoutes } from './http/routes.js'
 
 export const app = fastiy()
 
-app.post('/users', async (request, reply) => {
-  const registerBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  })
-  const { name, email, password } = registerBodySchema.parse(request.body)
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  })
-
-  return reply.status(201).send()
-})
+app.register(appRoutes)
