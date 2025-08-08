8e0cd6a6bcd07cff355aea138e6ec5449213b606
J-Breno <joao.breno85@hotmail.com>
2025-08-08 06:19:15 -0300

    Tratando env com zod

--- 02-api-rest-nodejs/src/database.ts
+++ 02-api-rest-nodejs/src/database.ts
@@ -1,14 +1,12 @@
 import 'dotenv/config'
 import setupKnex, { Knex} from 'knex'
+import { env } from './env/index.js'
 
-if(!process.env.DATABASE_URL) {
-    throw new Error('DABASE_URL env not found')
-}
 
 export const config: Knex.Config =  {
   client: 'sqlite',
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
