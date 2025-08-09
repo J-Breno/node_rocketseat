503592718a718a49f9e7f84d0ee0c4f0c7c87f8e
J-Breno <joao.breno85@hotmail.com>
2025-08-09 02:38:46 -0300

    Interface do repositório

--- 03-api-solid/src/http/controllers/register.ts
+++ 03-api-solid/src/http/controllers/register.ts
@@ -3,7 +3,7 @@ import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma.js'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import { RegisterUseCase } from '@/use-case/register.js'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository.js'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository.js'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
--- 03-api-solid/src/repositories/in-memory-users-repository.ts
+++
@@ -1,8 +0,0 @@
-import { Prisma } from 'generated/prisma/index.js'
-
-export class InMemoryUsersRepository {
-  public users: any[] = []
-  async create(data: Prisma.UserCreateInput) {
-    this.users.push(data)
-  }
-}
