e732b5e0b39b8d4b30ca0e7cc67e85d8348c36a2
J-Breno <joao.breno85@hotmail.com>
2025-08-09 02:27:36 -0300

    Inversão de dependências

--- 03-api-solid/src/http/controllers/register.ts
+++ 03-api-solid/src/http/controllers/register.ts
@@ -2,7 +2,8 @@ import { z } from 'zod'
 import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma.js'
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import { registerUseCase } from '@/use-case/register.js'
+import { RegisterUseCase } from '@/use-case/register.js'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository.js'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -13,7 +14,9 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    await registerUseCase({
+    const prismaUserRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUserRepository)
+    await registerUseCase.execute({
         email, name, password
     })
   } catch {
