1e1f032f6fdeca54959ad24a39ad86918c074e4f
J-Breno <joao.breno85@hotmail.com>
2025-08-10 12:57:17 -0300

    TDD & Mocking

--- 03-api-solid/src/repositories/check-ins-repository.ts
+++ 03-api-solid/src/repositories/check-ins-repository.ts
@@ -2,4 +2,5 @@ import type { CheckIn, Prisma } from '../../generated/prisma/index.js'
 
 export interface CheckInRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
--- 03-api-solid/src/use-case/check-in.spec.ts
+++ 03-api-solid/src/use-case/check-in.spec.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository.js'
 import { CheckInUseCase } from './check-in.js'
 
@@ -9,8 +9,49 @@ describe('Check-in Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
   })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
   it('Should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('Should not be able check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+
+  it('Should be able check in twice in different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
