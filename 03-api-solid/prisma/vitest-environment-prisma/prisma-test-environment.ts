d64c987db0ecd0010868721c2d5fd43d629bf8e7
J-Breno <joao.breno85@hotmail.com>
2025-08-11 03:24:40 -0300

    Test Environment do prisma

--- 03-api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ 03-api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,39 @@
+import { PrismaClient } from 'generated/prisma/client.js'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import type { Environment } from 'vitest'
+
+const prisma = new PrismaClient()
+
+function generateDatabasseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('Executou')
+    const schema = randomUUID()
+    const databaseURL = generateDatabasseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma db push')
 
     return {
-      async teardown() {},
+      async teardown() {
+        await prisma.$executeRawUnsafe(`
+            DROP SCHEMA IF EXISTS "${schema}" CASCADE
+        `)
+        await prisma.$disconnect()
+      },
     }
   },
 }
