b59b2da994ba140723d3560178275a14be140865
J-Breno <joao.breno85@hotmail.com>
2025-08-12 14:26:10 -0300

    Mapeando propriedades

--- 04-clean-ddd/src/domain/entities/question.ts
+++ 04-clean-ddd/src/domain/entities/question.ts
@@ -1,13 +1,17 @@
 import type { Slug } from "./value-objects/slug.js"
 import { Entity } from "../../core/entities/entity.js"
+import type { UniqueEntityId } from "../../core/entities/unique-entity-id.js"
 
 interface QuestionProps {
     title: string
     content: string
     slug: Slug
-    authorId: string
+    authorId: UniqueEntityId
+    bestAnswerId?: UniqueEntityId
+    createdAt: Date
+    updatedAt?: Date 
 }
 
 export class Question extends Entity<QuestionProps> {
-    
+
 }
