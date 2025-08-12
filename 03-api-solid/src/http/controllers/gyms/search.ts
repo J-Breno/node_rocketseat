6b2cc1278efb13bfe80ce5623413cf3f983274b8
J-Breno <joao.breno85@hotmail.com>
2025-08-12 09:18:29 -0300

    Criando controllers de academia para search e nearby

---
+++ 03-api-solid/src/http/controllers/gyms/search.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod'
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import { makeSearchGymsUseCase } from '@/use-case/factories/make-search-gyms-use-case.js'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const createGymQuerySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+  const { q, page } = createGymQuerySchema.parse(request.body)
+
+  const searchGymsUseCase = makeSearchGymsUseCase()
+  const { gyms } = await searchGymsUseCase.execute({
+    query: q,
+    page,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
