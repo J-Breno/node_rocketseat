97b2f80c473ee721ff3526d944356647def8f352
J-Breno <joao.breno85@hotmail.com>
2025-08-12 13:49:28 -0300

    Value Object de slug

--- 04-clean-ddd/src/domain/entities/question.ts
+++ 04-clean-ddd/src/domain/entities/question.ts
@@ -1,20 +1,24 @@
 import { randomUUID } from "node:crypto"
+import type { Slug } from "./value-objects/slug.js"
 
 interface QuestionProps {
     title: string
     content: string
-    authorId: string,
+    slug: Slug
+    authorId: string
 }
 
 export class Question {
     public id: string
     public title: string
+    public slug: Slug
     public content: string
     public authorId: string
 
-    constructor({title, content, authorId}: QuestionProps, id?: string) {
+    constructor({title, content,slug, authorId}: QuestionProps, id?: string) {
         this.title = title
         this.content = content
+        this.slug = slug
         this.authorId = authorId
         this.id = id ?? randomUUID()
     }
