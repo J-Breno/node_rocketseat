09a2878181b16434d3cee03f0ccc10b6895a3800
J-Breno <joao.breno85@hotmail.com>
2025-08-12 18:29:22 -0300

    Gerando dados fictícios

--- 04-clean-ddd/test/factories/make-question.ts
+++ 04-clean-ddd/test/factories/make-question.ts
@@ -4,14 +4,21 @@ import {
   type QuestionProps,
 } from '@/domain/forum/enterprise/entities/question.js'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug.js'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    title: 'Example Question',
-    slug: Slug.create('example-question'),
-    authorId: new UniqueEntityId(),
-    content: 'Example content',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+) {
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      slug: Slug.create('example-question'),
+      authorId: new UniqueEntityId(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
   return question
 }
