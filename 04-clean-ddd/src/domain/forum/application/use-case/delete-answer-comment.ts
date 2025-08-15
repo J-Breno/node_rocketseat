fbe31c18a3d6424a5f98572b227b17ee4fb612bc
J-Breno <joao.breno85@hotmail.com>
2025-08-15 19:25:03 -0300

    Criando erros genéricos

--- 04-clean-ddd/src/domain/forum/application/use-case/delete-answer-comment.ts
+++ 04-clean-ddd/src/domain/forum/application/use-case/delete-answer-comment.ts
@@ -1,12 +1,17 @@
 import { left, right, type Either } from '@/core/either.js'
 import type { AnswerCommentRepository } from '../repositories/answer-comments-repository.js'
+import { ResourceNotFoundError } from './errors/resource-not-found-error.js'
+import { NotAllowedError } from './errors/not-allowed-error.js'
 
 interface DeleteAnswerCommentUseCaseRequest {
   authorId: string
   answerCommentId: string
 }
 
-type DeleteAnswerCommentUseCaseResponse = Either<string, {}>
+type DeleteAnswerCommentUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {}
+>
 
 export class DeleteAnswerCommentUseCase {
   constructor(private answerCommentRepository: AnswerCommentRepository) {}
@@ -19,11 +24,11 @@ export class DeleteAnswerCommentUseCase {
       await this.answerCommentRepository.findById(answerCommentId)
 
     if (!answerComment) {
-      return left('Answer comment not found.')
+      return left(new ResourceNotFoundError())
     }
 
     if (answerComment.authorId.toString() !== authorId) {
-      return left('Not allowed')
+      return left(new NotAllowedError())
     }
 
     await this.answerCommentRepository.delete(answerComment)
