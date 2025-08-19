a7cb4fcd1a3c171ca11d2639bb1a063aa42824f5
J-Breno <joao.breno85@hotmail.com>
2025-08-19 03:00:07 -0300

    trabalhando com pipes

--- 06-curso-nestjs/src/models/users/user.controller.ts
+++ 06-curso-nestjs/src/models/users/user.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { CreateUserService } from './create-user.service';
 import type { CreateUserDTO } from './dto/user.dto';
+import { CreateUserValidationPipe } from './pipe/create-user.validation.pipe';
 
 @Controller('/users')
 export class UserController {
   constructor(private readonly createUserUseCase: CreateUserService) {}
 
   @Post()
+  @UsePipes(new CreateUserValidationPipe())
   async create(@Body() data: CreateUserDTO) {
     return await this.createUserUseCase.execute(data);
   }
