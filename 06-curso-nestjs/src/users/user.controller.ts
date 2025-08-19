9c3f7cfd1463d2c578972cfd52355cf142c61249
J-Breno <joao.breno85@hotmail.com>
2025-08-19 02:03:35 -0300

    Conhecendo parametros body

--- 06-curso-nestjs/src/users/user.controller.ts
+++ 06-curso-nestjs/src/users/user.controller.ts
@@ -1,13 +1,38 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
 
 interface ParamUser {
   id: string;
+  idEmpresa: string;
 }
 
-@Controller()
+interface QueryUser {
+  p: string;
+  r: string;
+}
+
+interface BodyUser {
+  name: string;
+  age: number;
+}
+
+@Controller('/users')
 export class UserController {
-  @Get('/users/:id')
-  findById(@Param() { id }: ParamUser) {
-    return 'Usuário do ID ' + id;
+  @Get('/:id/:idEmpresa')
+  findById(@Param() { id, idEmpresa }: ParamUser) {
+    return 'Usuário do ID ' + id + ' - Empresa id: ' + idEmpresa;
+  }
+
+  @Get('/findByPages')
+  findByPages(@Query() { p, r }: QueryUser) {
+    return 'Query p ' + p + ' Query r ' + r;
+  }
+
+  @Post('/create')
+  create(@Body() data: BodyUser) {
+    return {
+      ...data,
+      id: randomUUID(),
+    };
   }
 }
